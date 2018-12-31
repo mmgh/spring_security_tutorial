78aed6cfe39043ca1521ab8f183c1789c947fe0a
mmgh <forstill@gmail.com>
2018-12-31 20:08:56 +0900

    userDetailService로 분리처리

--- src/main/java/com/spring/app/security/service/CustomUserDetailService.java
+++ src/main/java/com/spring/app/security/service/CustomUserDetailService.java
@@ -1,4 +1,4 @@
-package com.spring.app.security.dao;
+package com.spring.app.security.service;
 
 import java.util.Collection;
 import java.util.List;
@@ -10,69 +10,68 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
-public class AppUserDetailServiceDAO implements UserDetailsService{
+public class CustomUserDetailService implements UserDetailsService{
 
 	protected final Log logger = LogFactory.getLog(getClass());
 	
 	@Override
-	public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		
 		logger.info("loadUserByUsername username : " + username);
+		
 		
 		if(!username.equals("admin")) {
 			throw new UsernameNotFoundException(username + " not found");
 		}
 		
-		//creating dummy user details, should do JDBC opertion
+		
+		// creating dummy user details, should do JDBC opertion
 		return new UserDetails() {
-			
+
 			private static final long serialVersionUID = 1181386790787910601L;
 
 			@Override
 			public boolean isEnabled() {
 				return true;
 			}
-			
+
 			@Override
 			public boolean isCredentialsNonExpired() {
 				return true;
 			}
-			
+
 			@Override
 			public boolean isAccountNonLocked() {
 				return true;
 			}
-			
+
 			@Override
 			public boolean isAccountNonExpired() {
 				return true;
 			}
-			
+
 			@Override
 			public String getUsername() {
 				return username;
 			}
-			
+
 			@Override
 			public String getPassword() {
 				return "admin123";
 			}
-			
+
 			@Override
 			public Collection<? extends GrantedAuthority> getAuthorities() {
-				
 				List<SimpleGrantedAuthority> auth = new java.util.ArrayList<SimpleGrantedAuthority>();
 				auth.add(new SimpleGrantedAuthority("Admin"));
 				return auth;
 			}
-			
+
 		};
-		
-		
-		
-		
-		
+
+				
 	}
 
 }
