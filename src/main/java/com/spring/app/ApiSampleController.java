7af952b484f1929188f8baa6876b6332613764e4
mmgh <forstill@gmail.com>
2020-04-27 10:32:32 +0900

    aligned.

--- src/main/java/com/spring/app/ApiSampleController.java
+++ src/main/java/com/spring/app/ApiSampleController.java
@@ -15,10 +15,12 @@ public class ApiSampleController {
 	
 	@RequestMapping(value = "/secured/scrId/apiSample", method= {RequestMethod.POST, RequestMethod.GET})
 	public Map apiSampleMethod(@RequestBody Map<String, Object> requestData) {
+		
 		Map<String, Object> responseData = new HashMap<String, Object>();
 		responseData.put("data", requestData);
 		return responseData;
 	}
 	
 }
+
 
